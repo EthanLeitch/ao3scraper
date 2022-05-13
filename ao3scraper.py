92998fb4af64590a9252c7c681194b0983ba7b08
Ethan <ethanjohnleitch@gmail.com>
2022-05-13 21:31:32 +1200

    :ambulance: Scanning now works as intended
    Fixed scanning features

--- ao3scraper.py
+++ ao3scraper.py
@@ -15,13 +15,20 @@ from rich.progress import track
 
 import click
 
+# Setup fic_array item constants
+URL_POS = 0
+TITLE_POS = 1
+CHAPTER_POS = 2
+LAST_UPDATED_POS = 3
+
 # Create columns of rich table
 table = Table(title="Fanfics")
 
 table.add_column("Index", justify="left", style="red", no_wrap=True)
 table.add_column("Title", style="magenta")
-table.add_column("Last updated", style="cyan", no_wrap=True)
-table.add_column("Chapter", justify="left", style="green")
+table.add_column("Chapter",  style="green")
+table.add_column("Last updated", justify="left", style="cyan", no_wrap=True)
+
 
 # Create database file if database does not exist
 if not os.path.exists("fics.db"):
@@ -31,7 +38,7 @@ if not os.path.exists("fics.db"):
     connection = sqlite3.connect("fics.db")
 
     cursor = connection.cursor()
-    cursor.execute("CREATE TABLE fics (title TEXT, url TEXT, updated TEXT, chapters TEXT)")
+    cursor.execute("CREATE TABLE fics (url TEXT, title TEXT, chapters TEXT, updated TEXT)")
     
     connection.commit()
     connection.close()
@@ -40,16 +47,16 @@ if not os.path.exists("fics.db"):
 
 # Start click command
 @click.command()
-@click.option('--scan', '-s', is_flag=True, help='Launches scanning mode.')
+@click.option('--scrape', '-s', is_flag=True, help='Launches scraping mode.')
 @click.option('--list', '-l', is_flag=True, help='Lists all entries in the database.')
 @click.option('--add', '-a', is_flag=True, help='Opens a text file to add multiple urls to the database.')
 #@click.option('--add', '-a', help='Adds a single url to the database.')
 #@click.option('--add-urls', is_flag=True, help='Opens a text file to add multiple urls to the database.')
-@click.option('--delete', '-d', help='Deletes an entry from the database.')
+@click.option('--delete', '-d', help='Deletes an entry from the database.', type=int)
 
-def main(scan, add, list, delete):
-    if scan:
-        scan_urls()
+def main(scrape, add, list, delete):
+    if scrape:
+        scrape_urls()
     elif list:
         construct_rich_table()
     elif add:
@@ -68,7 +75,7 @@ def main(scan, add, list, delete):
     exit()
 
 
-def scan_urls():
+def scrape_urls():
     # Check if AO3 is online / accessible
     print("Checking if AO3 servers are online...")
     try:
@@ -79,8 +86,56 @@ def scan_urls():
     else:
         print("Contacted servers successfully.")
 
-    for item in fic_table:
-        print(item[1])
+    # Handle each url
+    for item in track(fic_table, description="Fetching data from AO3..."):
+        # Fetch all external chapter values of URLS
+        web_tags = get_tags(item[URL_POS], item)
+
+        # Get item index
+        item_index = str(fic_table.index(item) + 1) + "."
+
+        # Check if url has local tags: title, chapters, last updated
+        if "title" and "chapters" and "updated" not in item:
+            table.add_row(item_index, "[link=" + item[URL_POS] + "]" + web_tags[0]+ "[/link]", web_tags[1], web_tags[2])
+        else:
+            # Compare each local chapter value to each web chapter value
+            if int(web_tags[1].split("/")[0]) > int(item[CHAPTER_POS].split("/")[0]):
+                table.add_row("[#ffcc33][bold]" + item_index,
+                            "[#ffcc33][bold][link=" + item[URL_POS] + "]" + web_tags[0] + "[/link]",
+                            "[#ffcc33][bold]" + web_tags[1],
+                            "[#ffcc33][bold]" + web_tags[2])
+            else:
+                table.add_row(item_index, "[link=" + item[URL_POS] + "]" + web_tags[0] + "[/link]", web_tags[1], web_tags[2])
+
+        # Write item information to database
+        target_entry = fic_table[fic_table.index(item)]
+        #print("UPDATE fics SET title = '" + web_tags[0] + "', chapters = '" + web_tags[1] + "', updated = '" + web_tags[2] + "' WHERE url = '" + target_entry[URL_POS] + "';")
+        cursor.execute("UPDATE fics SET title = \"" + web_tags[0] + "\", chapters = \"" + web_tags[1] + "\", updated = \"" + web_tags[2] + "\" WHERE url = \"" + target_entry[URL_POS] + "\";")
+        connection.commit()
+
+    # Print rich table
+    print()
+    console = Console()
+    console.print(table)
+
+# Function to fetch all online tags for one URL
+def get_tags(url, item):
+    page = requests.get(url)
+
+    # 4xx and 5xx Error Detection
+    if not page.ok:
+        print(page.status_code, "Error!", style="red")
+        print("URL responsible: ", item[URL_POS])
+        pass
+        # continue would work here? but not in a loop
+
+    soup = BeautifulSoup(page.content, "html.parser")
+
+    # Return tags in order: title, chapters, last updated.
+    return[soup.find_all(class_="title heading")[0].string.strip(),
+           soup.find_all(class_="chapters")[1].string,
+           soup.find_all(class_="status")[1].string]
+
 
 def construct_rich_table():
     # Refresh table entries
@@ -88,21 +143,27 @@ def construct_rich_table():
     fic_table = cursor.fetchall()
 
     for item in fic_table:
+            """
+            item_id = str(item[1])
+            item_id = item_id.split("archiveofourown.org/works/")[1] # Remove everything before and including this string
+            item_id = item_id.split("/", 1)[0] # Remove everything after and including this string
+            """
 
-            #item_id = str(item[1])
-            #item_id = item_id.split("archiveofourown.org/works/")[1] # Remove everything before and including this string
-            #item_id = item_id.split("/", 1)[0] # Remove everything after and including this string
+            # Get item index
+            item_index = str(fic_table.index(item) + 1) + "."
 
-            # Check if url field is empty
-            if item[0] == None:
-                table.add_row(str(fic_table.index(item) + 1) + ".", "[link=" + item[1] + "]FIC NOT YET SCANNED[/link]", item[2], item[3])
+            # Check if title field is empty
+            if item[TITLE_POS] == None:
+                table.add_row(item_index, "[link=" + item[URL_POS] + "]FIC DATA NOT YET SCRAPED[/link]", item[CHAPTER_POS], item[LAST_UPDATED_POS])
             else:
                 # table.add_row(str(fic_table.index(item) + 1) + ". [link=" + item[1] + "]" + item_id + "[/link]", item[0], item[2], item[3])
-                table.add_row(str(fic_table.index(item) + 1) + ".", "[link=" + item[1] + "]" + item[0] + "[/link]", item[2], item[3])
+                table.add_row(item_index, "[link=" + item[URL_POS] + "]" + item[TITLE_POS] + "[/link]", item[CHAPTER_POS], item[LAST_UPDATED_POS])
 
+    # Print rich table
     print()
     console = Console()
     console.print(table)
+
 
 def add_urls():
     MARKER = "# Enter one url on each line to add it to the database. This line will not be recorded."
@@ -114,20 +175,21 @@ def add_urls():
         for i in url_to_parse:
             cursor.execute("INSERT INTO fics (url) VALUES ('" + str(i) + "');")
             connection.commit()
-            print("Added " + url_to_parse[url_to_parse.index(i)]) # fix
+            print("Added " + url_to_parse[url_to_parse.index(i)]) # improve this logic
 
     construct_rich_table()
 
+
 def delete_entry(entry):
     try:
-        target_entry = fic_table[(int(entry) - 1)] # black magic fuckery, consider using rowid
-        cursor.execute("DELETE FROM fics WHERE url = '" + target_entry[1] + "';")
+        target_entry = fic_table[(entry - 1)] # possible black magic, consider using rowid
+        cursor.execute("DELETE FROM fics WHERE url = '" + target_entry[URL_POS] + "';")
+        #cursor.execute("DELETE FROM fics WHERE rowid = " + entry + ";")
     except IndexError:
         print("Number out of index range.")
         exit()
-    #cursor.execute("DELETE FROM fics WHERE rowid = " + entry + ";")
     connection.commit()
-    print("Deleted entry number " + entry)
+    print("Deleted entry number " + str(entry))
 
     construct_rich_table()
 
@@ -142,50 +204,3 @@ if __name__ == "__main__":
     fic_table = cursor.fetchall()
 
     main()
-
-"""
-# Function to fetch all online tags for one URL
-def get_tags(url):
-    page = requests.get(url)
-
-    # 4xx and 5xx Error Detection
-    if not page.ok:
-        print(page.status_code, "Error!")
-        print("URL responsible: ", i["url"])
-        pass
-
-    soup = BeautifulSoup(page.content, "html.parser")
-
-    # Return tags in order: title, last updated, chapters
-    return[soup.find_all(class_="title heading")[0].string.strip(),
-           soup.find_all(class_="status")[1].string,
-           soup.find_all(class_="chapters")[1].string]
-
-
-# Handle each url
-for i in track(urls, description="Fetching data from AO3..."):
-    # Fetch all external chapter values of URLS
-    web_tags = get_tags(i["url"])
-
-    # Check if url has local tags: title, last updated, chapters
-    if "title" and "updated" and "chapters" not in i:
-        table.add_row(web_tags[1], "[link=" + i["url"] + "]" + web_tags[0] + "[/link]", web_tags[2])
-    else:
-        # Compare each local chapter value to each web chapter value
-        if int(web_tags[2].split("/")[0]) > int(i["chapters"].split("/")[0]):
-            table.add_row("[#ffcc33][bold]" + web_tags[1],
-                          "[#ffcc33][bold][link=" + i["url"] + "]" + web_tags[0] + "[/link]",
-                          "[#ffcc33][bold]" + web_tags[2])
-        else:
-            table.add_row(web_tags[1], "[link=" + i["url"] + "]" + web_tags[0] + "[/link]", web_tags[2])
-
-    # Update url information
-    i["title"] = web_tags[0]
-    i["updated"] = str(web_tags[1])
-    i["chapters"] = str(web_tags[2])
-
-    # Write to yaml file
-    with open("fics.yaml", "w") as file:
-        yaml.dump(config, file)
-    file.close()
-"""
